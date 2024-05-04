67a29e339597b4e35777d9ff64bc51136b2d7cb5
Arpit Gupta <Arpit.Gupta1@gds.ey.com>
2024-05-05 00:49:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function handleNavIconClick(item) {
+    $(`#${item}`)[0].scrollIntoView();
+}
+
+function handleContactIconClick(item) {
+    window.open(
+        item === "gmail"
+            ? "mailto:er.gupta.arpit@gmail.com"
+            : item === "linkedin"
+                ? "https://www.linkedin.com/in/er-arpit-gupta"
+                : "https://github.com/er-arpitgupta"
+    );
+}
+
+function handleFilterClick(item) {
+    $('.filters label').removeClass('active')
+    $(`.filters label[for="${item}"]`).addClass('active')
+}
