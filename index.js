f55293c13aea598460748424d38188f17dcb09c2
Arpit Gupta <Arpit.Gupta1@gds.ey.com>
2024-05-05 02:02:44 +0530

    code added

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function handleFilterClick(item) {
     $('.filters label').removeClass('active')
     $(`.filters label[for="${item}"]`).addClass('active')
 }
+
+function handleImageClick(image) {
+    $('.popup img').attr('src', `images/high_images/${image}.jpg`);
+    $('.popup').css('display', 'flex');
+}
+
+function handlePopupClose() {
+    $('.popup').css('display', 'none');
+}
